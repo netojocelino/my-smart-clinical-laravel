7830b88a13c4832edb2c2870fabae3486dd11628
Jocelino Neto <netoj96@live.com>
2023-12-13 12:28:58 -0300

    tests(red): Create a test to fail

---
+++ tests/Feature/CreatePublicTodoItemTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TodoItem;
+use Illuminate\Http\JsonResponse;
+use Tests\TestCase;
+
+class CreatePublicTodoItemTest extends TestCase
+{
+    public function testShouldCreateAItemForValidData(): void
+    {
+        // Arrange
+        $data = [
+            'title'       => 'My first task',
+            'description' => 'This is my first task with some description',
+            'status'      => 'pendent',
+        ];
+
+        // act
+        $response = $this->post(route('app.todo.item.store', $data));
+
+        // assert
+        $response->assertStatus(JsonResponse::HTTP_CREATED);
+        $this->assertDatabaseHas(TodoItem::class, [
+            'title'       => data_get($data, 'title'),
+            'description' => data_get($data, 'description'),
+            'status'      => data_get($data, 'status'),
+            'completed_at' => null,
+        ]);
+    }
+}
